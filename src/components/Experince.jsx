887b09d02361434f141709c8e81cc7660e04b227
Yuvraj Shekhar <syuvraj1999@gmail.com>
2025-01-21 10:30:38 +0100

    fixed experience title spelling mistake

--- src/components/Experince.jsx
+++ src/components/Experince.jsx
@@ -9,7 +9,7 @@ const Experince = () => {
         whileInView={{opacity:1, y:0 }}
         initial={{opacity:0, y:-100}}
         transition={{duration:0.5}}
-        className='my-20 text-center text-4xl'>Experince</motion.h1>
+        className='my-20 text-center text-4xl'>Experience</motion.h1>
         <div>
             {EXPERIENCES.map((experince, index) => (
                 <motion.div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
